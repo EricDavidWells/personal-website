28d2ba17a8d76edf08a7e432f2cf6cf5ac55e698
eric.wells <ericwellsrobotics@gmail.com>
2024-12-08 16:10:27 -0800

    progress

--- eric-personal-website/src/app/shared/constants.ts
+++ eric-personal-website/src/app/shared/constants.ts
@@ -1,14 +1,60 @@
 import { Project } from './project';
 
-export const PROJECTS: Project[] = 
-[
+export const PROJECTS: Project[] = [
   new Project(
-      'blobs',
-      'Blob Neuroevolution Simulation', 
-      '2017-04',
-      '/assets/images/blobs_icon.png',
-      `Blob's battle for survival while evolving through genetic algorithms.`
+    'multi-myosharp',
+    'Multi-Myosharp',
+    '2021-02',
+    'assets/images/multi-myosharp-icon.png',
+    `An electro-myography wristband's data acquisition strategy is improved.`
   ),
-  new Project('project-two', 'Project Two', '2023-02-15', 'path/to/image2.jpg', 'A brief description of Project Two.'),
-]
-
+  new Project(
+    'advent-of-code',
+    'Advent of Code',
+    '2020-12',
+    'assets/images/advent-of-code-icon.png',
+    `Programming puzzles are completed to improve skills.`
+  ),
+  new Project(
+    'esp32-latency',
+    'ESP32 Latency Testing',
+    '2020-08',
+    'assets/images/esp32-latency-icon.png',
+    `Latency and throughput are evaluated on an ESP32 microcontroller.`
+  ),
+  new Project(
+    'compliant-sensors',
+    'Compliant Fingertip Sensors',
+    '2020-03',
+    'assets/images/compliant-sensors-icon.png',
+    `Capacitive force sensors are evaluated for prosthetics.`
+  ),
+  new Project(
+    'polyvolve',
+    'Polyvolve',
+    '2020-01',
+    'assets/images/polyvolve-icon.png',
+    `A genetic algorithm draws pictures using transparent polygons.`
+  ),
+  new Project(
+    'axis-reloaded',
+    'Axis Reloaded',
+    '2019-09',
+    'assets/images/axis-reloaded-icon.png',
+    `A 15-DOF robotic claw rotates a platform continuously with self-correction.`
+  ),
+  new Project(
+    'force-myography',
+    'Wireless Force Myography',
+    '2019-05',
+    'assets/images/force-myography-icon.png',
+    `A wireless arm band measuring muscle deformation controls a prosthesis.`
+  ),
+  new Project(
+    'blobs',
+    'Blob Neuroevolution Simulation',
+    '2017-04',
+    'assets/images/blobs-icon.png',
+    `Blob's battle for survival while evolving through genetic algorithms.`
+  ),
+];
