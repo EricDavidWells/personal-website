48d5c53e016f0f0af1041c9e7204dc41afc0782c
eric.wells <ericwellsrobotics@gmail.com>
2024-12-15 13:50:09 -0800

    add pictures

--- eric-personal-website/src/app/components/home/home.component.ts
+++ eric-personal-website/src/app/components/home/home.component.ts
@@ -8,7 +8,33 @@ import { Component, OnInit, signal } from '@angular/core';
 })
 export class HomeComponent implements OnInit {  
 
-  ngOnInit(): void {
+  public slides: string [] = [
+    './assets/images/mugshot_ds.png',
+    './assets/images/personal/cenotes.jpg', 
+    './assets/images/personal/EricClimbNordegg.png',
+    './assets/images/personal/EricPointCloudArm.png',
+    // './assets/images/personal/EricDive.png', 
+    './assets/images/personal/eric-multipitch.png',
+  ]
+  i: number = 0;
+
+  showSlide(slides: string [], i: number) {
+      let slide = slides[i];
+      return slide;
+  }
+
+  getPrev(slides: string [], i: number) {
+      this.i = (this.i - 1 + slides.length) % slides.length;
+      this.showSlide(slides, i)
+  }
+
+  getNext(slides: string [], i: number) {
+    this.i = (this.i + 1) % slides.length;
+    this.showSlide(slides, i)
+  }
+
+  ngOnInit() {
+    this.i = 0;
   }
 
 }
